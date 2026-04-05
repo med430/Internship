937e7af76a6a5f2c70b61dc653abea9a0825ccca
med430 <medps4fr@gmail.com>
2026-04-05 03:55:25 +0200

    small changes

--- Internship/backend/src/Infrastructure/Persistence/prisma/mappers/user.mapper.ts
+++ Internship/backend/src/Infrastructure/Persistence/prisma/mappers/user.mapper.ts
@@ -1,18 +1,18 @@
-import {User} from "../../../../Domain/user.entity";
+import {User} from "../../../../Domain/entities/user.entity";
 
 export class UserPrismaMapper {
     static toDomain(user: any): User {
-        return new User(user.id, user.firstName, user.lastName, user.username, user.email, user.password, user.role);
+        return new User(user.id, user.name, user.lastname, user.username, user.email, user.password, user.role);
     }
 
     static toPersistence(user: User) {
         return {
             id: user.id,
-            firstName: user.firstName,
-            lastName: user.lastName,
+            firstName: user.name,
+            lastName: user.lastname,
             username: user.username,
             email: user.email,
-            password: user.password,
+            password: user.passwordHash,
             role: user.role,
         };
     }
