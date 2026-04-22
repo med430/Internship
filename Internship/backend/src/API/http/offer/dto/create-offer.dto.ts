cfa0cfe996eca9577bf38d73ad7060e29dd428fc
Samira <alisamira.bengara@insat.ucar.tn>
2026-04-22 16:32:33 +0100

    modification de dto createoffer

--- Internship/backend/src/API/http/offer/dto/create-offer.dto.ts
+++ Internship/backend/src/API/http/offer/dto/create-offer.dto.ts
@@ -3,6 +3,14 @@ import {OfferType} from "../../../../Domain/enums/offer-type.enum";
 export class CreateOfferDTO {
     title: string
     description: string
+
+    company: string
+    location: string
+    domain: string
+
+    startDate: Date
+    endDate: Date
+
     type: OfferType
 
     requiredSkills: {
