468cd878ea5ebde15d1b7378231bf4c276465b33
Samira <alisamira.bengara@insat.ucar.tn>
2026-04-22 16:31:17 +0100

    modification de l 'entite offer

--- Internship/backend/src/Domain/entities/offer.entity.ts
+++ Internship/backend/src/Domain/entities/offer.entity.ts
@@ -1,15 +1,26 @@
-import {SkillAssignment } from './skill-assignment.entity'
-import {OfferType} from "../enums/offer-type.enum";
-import {BaseEntity} from "./base.entity";
+import { SkillAssignment } from './skill-assignment.entity';
+import { OfferType } from "../enums/offer-type.enum";
+import { BaseEntity } from "./base.entity";
 
 export class Offer extends BaseEntity {
     constructor(
         public readonly id: string,
         public readonly creatorId: string,
+
         public title: string,
         public description: string,
+
+        public company: string,
+        public location: string,
+
+        public domain: string,
+
+        public startDate: Date,
+        public endDate: Date,
+
         public requiredSkills: SkillAssignment[],
         public type: OfferType,
+
         createdAt?: Date,
         updatedAt?: Date,
         deletedAt?: Date,
