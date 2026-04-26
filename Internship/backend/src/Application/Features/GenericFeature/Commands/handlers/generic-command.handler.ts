bcd1647975ff369a7534accde7023bb9ac753b08
med430 <medps4fr@gmail.com>
2026-04-26 22:08:02 +0200

    added a simple generic command following the template design pattern it may help decrease a little of the repetitive code

---
+++ Internship/backend/src/Application/Features/GenericFeature/Commands/handlers/generic-command.handler.ts
@@ -0,0 +1,34 @@
+import { ICommandHandler } from '@nestjs/cqrs';
+
+export abstract class GenericCommandHandler<
+  TCommand,
+  TEntity,
+  TResult,
+> implements ICommandHandler<TCommand> {
+  async execute(command: TCommand): Promise<TResult> {
+    await this.validate(command);
+
+    const entity = await this.map(command);
+
+    await this.beforePersist(entity);
+
+    const result = await this.persist(entity);
+
+    await this.afterPersist(result, command);
+
+    return result;
+  }
+
+  protected async validate(command: TCommand): Promise<void> {}
+
+  protected async beforePersist(entity: TEntity): Promise<void> {}
+
+  protected abstract map(command: TCommand): Promise<TEntity>;
+
+  protected abstract persist(entity: TEntity): Promise<TResult>;
+
+  protected async afterPersist(
+    result: TResult,
+    command: TCommand,
+  ): Promise<void> {}
+}
