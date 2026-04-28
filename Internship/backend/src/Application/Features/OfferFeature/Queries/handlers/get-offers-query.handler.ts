e8606813f8be32f23a45f301c32dc4e2658e1362
Samira <alisamira.bengara@insat.ucar.tn>
2026-04-28 16:36:47 +0100

    Resolution des erreurs

--- Internship/backend/src/Application/Features/OfferFeature/Queries/handlers/get-offers-query.handler.ts
+++ Internship/backend/src/Application/Features/OfferFeature/Queries/handlers/get-offers-query.handler.ts
@@ -1,14 +1,17 @@
-import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
-import { GetOffersQuery } from '../get-offers.query';
-import { IOfferRepository } from '../../../../repositories/offer.repository';
-import { Offer } from '../../../../../Domain/entities/offer.entity';
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
+import { Inject } from '@nestjs/common'                    // ← ajouter
+import { GetOffersQuery } from '../get-offers.query'
+import { IOfferRepository } from '../../../../repositories/offer.repository'
+import { Offer } from '../../../../../Domain/entities/offer.entity'
 
 @QueryHandler(GetOffersQuery)
 export class GetOffersQueryHandler implements IQueryHandler<GetOffersQuery> {
-  constructor(private readonly offerRepository: IOfferRepository) {
-  }
+  constructor(
+      @Inject(IOfferRepository)                              // ← ajouter
+      private readonly offerRepository: IOfferRepository
+  ) {}
 
   async execute(query: GetOffersQuery): Promise<Offer[]> {
-    return this.offerRepository.findPaginated(query.pageNumber, query.pageSize);
+    return this.offerRepository.findPaginated(query.pageNumber, query.pageSize)
   }
 }
